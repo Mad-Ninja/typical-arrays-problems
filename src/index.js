e8f5d50d2c8d2394baeee665520dc52a81f3a68b
Alexander <sharodyu@gmail.com>
2020-10-03 15:00:25 +0300

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
+    if (typeof array != "undefined"
+    && array != null
+    && array.length != null
+    && array.length > 0){
+        return Math.min(...array);
+    }else{
+        return 0;
+    }
+        
+    
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array != "undefined"
+    && array != null
+    && array.length != null
+    && array.length > 0){
+        return Math.max(...array);
+    }else{
+        return 0;
+    }
+   
+        
+    
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array != "undefined"
+    && array != null
+    && array.length != null
+    && array.length > 0){
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        return total / array.length;
+    }else{
+        return 0;
+    }
+    
+    
+       
 }
